bbaae5398806c3983cd3efee874ebaa21fa6ce05
Ahmed Mosaad <ahmedmosaad@example.com>
2026-03-27 10:57:05 +0300

    feat: auto-update source & submodule | 2026-03-27 10:56 | Mac

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
 
     const plugins = [
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         vue({
